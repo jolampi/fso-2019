fd2ad0307b50ae5d2fbac2fafd37599aaa8d978f
jolampi <36279434+jolampi@users.noreply.github.com>
2019-12-11 00:07:39 +0200

    Bloglist frontend: can sign in and out

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Blog from './components/Blog'
+import LoginForm from './components/LoginForm';
+
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [user, setUser] = useState(null)
+    const [blogs, setBlogs] = useState([])
+
+    const handleLogin = async (event) => {
+        event.preventDefault()
+        try {
+            const user = await loginService.login({ username, password })
+            window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+            setUser(user)
+            setUsername("")
+            setPassword("")
+        } catch(exception) {
+            console.log('wrong credentials')
+        }
+    }
+
+    const handleLogout = () => {
+        window.localStorage.clear()
+        setUser(null)
+    }
+
+    useEffect(() => {
+        const loadBlogs = (async () => {
+            const blogs = await blogService.getAll()
+            setBlogs(blogs)
+        })
+        loadBlogs()
+    }, [])
+
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON)
+            setUser(user)
+        }
+    }, [])
+
+    return (user === null) ? (
+        <div>
+            <h2>Log in to application</h2>
+            <LoginForm 
+                onSubmit={handleLogin}
+                username={username}
+                setUsername={setUsername}
+                password={password}
+                setPassword={setPassword}
+            />
+        </div>
+    ) : (
+        <div>
+            <h2>blogs</h2>
+            <p>
+                {user.name} logged in
+                <button onClick={handleLogout}>logout</button>
+            </p>
+            {blogs.map(blog =>
+                <Blog key={blog.id} blog={blog} />
+            )}
+        </div>
+    )
 }
 
 export default App;
