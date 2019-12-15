17db469bb7d011e9a614c3a2b68a1e5f0e091731
jolampi <36279434+jolampi@users.noreply.github.com>
2019-12-15 04:28:58 +0200

    Part 5: Ultimate hooks

--- osa5/ultimate_hooks/src/App.js
+++ osa5/ultimate_hooks/src/App.js
@@ -19,14 +19,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    setResources(response.data)
+  }
 
-  const create = (resource) => {
-    // ...
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
   }
 
   const service = {
-    create
+    create,
+    getAll
   }
 
   return [
@@ -41,6 +46,12 @@ const App = () => {
 
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
+
+  useEffect(() => {
+    noteService.getAll()
+    personService.getAll()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
