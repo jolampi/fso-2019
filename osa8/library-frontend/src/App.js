422621542b1436c2fd33959fdac3e6fdb087f6dc
jolampi <36279434+jolampi@users.noreply.github.com>
2019-12-29 19:35:18 +0200

    Part 8: Frontend for library

--- osa8/library-frontend/src/App.js
+++ osa8/library-frontend/src/App.js
@@ -1,7 +1,59 @@
 import React, { useState } from 'react'
+import { gql } from 'apollo-boost'
+import { ApolloConsumer, Mutation, Query } from 'react-apollo'
+
 import Authors from './components/Authors'
+import EditAuthor from './components/EditAuthor'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+{
+    allAuthors {
+        name
+        born
+        bookCount
+    }
+}
+`
+
+const ALL_BOOKS = gql`
+{
+    allBooks {
+        title
+        author
+        published
+    }
+}
+`
+
+const CREATE_BOOK = gql`
+mutation createBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+    addBook(
+        title: $title,
+        published: $published,
+        author: $author,
+        genres: $genres
+    ) {
+        title
+        published
+        author
+        genres
+    }
+}
+`
+
+const EDIT_AUTHOR = gql`
+mutation editAuthor($name: String!, $setBornTo: Int!) {
+    editAuthor(
+        name: $name,
+        setBornTo: $setBornTo
+    ) {
+        name
+        born
+    }
+}
+`
 
 const App = () => {
     const [page, setPage] = useState('authors')
@@ -13,19 +65,45 @@ const App = () => {
                 <button onClick={() => setPage('books')}>books</button>
                 <button onClick={() => setPage('add')}>add book</button>
             </div>
-
-            <Authors
-                show={page === 'authors'}
-            />
-
-            <Books
-                show={page === 'books'}
-            />
-
-            <NewBook
-                show={page === 'add'}
-            />
-
+            <ApolloConsumer>
+                {client => <div>
+                    <Query query={ALL_AUTHORS}>
+                        {result =>
+                            <div>
+                                <Authors
+                                    show={page === 'authors'}
+                                    result={result}
+                                />
+                                <Mutation
+                                    mutation={EDIT_AUTHOR}
+                                    refetchQueries={[{ query: ALL_AUTHORS }]}
+                                >
+                                    {editAuthor => <EditAuthor
+                                        show={page === 'authors'}
+                                        result={result}
+                                        editAuthor={editAuthor}
+                                    />}
+                                </Mutation>
+                            </div>
+                        }
+                    </Query>
+                    <Query query={ALL_BOOKS}>
+                        {result => <Books 
+                            show={page === 'books'}
+                            result={result}
+                            />}
+                    </Query>
+                    <Mutation
+                        mutation={CREATE_BOOK}
+                        refetchQueries={[{ query: ALL_AUTHORS }, { query: ALL_BOOKS }]}
+                    >
+                        {addBook => <NewBook
+                            show={page === 'add'}
+                            addBook={addBook}
+                        />}
+                    </Mutation>
+                </div>}
+            </ApolloConsumer>
         </div>
     )
 }
---
+++ osa8/library-frontend/src/components/EditAuthor.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const EditAuthor = (props) => {
+    const [name, setName] = useState({value: ''})
+    const [born, setBorn] = useState('')
+
+    if (!props.show || props.result.loading) {
+        return null
+    }
+
+    const submit = async (event) => {
+        event.preventDefault()
+        await props.editAuthor({
+            variables: { name: name.value, setBornTo: Number(born) }
+        })
+        setName('')
+        setBorn('')
+    }
+
+    return (
+        <div>
+            <h2>set birthyear</h2>
+            <form onSubmit={submit}>
+                <div>
+                    name
+                    <select value={name} onChange={({target}) => setName({value: target.value})}>
+                        {props.result.data.allAuthors.map(author =>
+                            <option key={author.name} value={author.name}>{author.name}</option>
+                        )}
+                    </select>
+                </div>
+                <div>
+                    born
+                    <input
+                        type="number"
+                        value={born}
+                        onChange={({ target }) => setBorn(target.value)} />
+                </div>
+                <button type="submit">update author</button>
+            </form>
+        </div>
+    )
+}
+
+export default EditAuthor
